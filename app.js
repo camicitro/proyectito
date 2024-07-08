c96042a38725fc634de5c5c23d81a19d7d36adf1
Camila <camilacitro1106@gmail.com>
2024-07-08 14:30:07 -0300

    Arregle eso

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { dbConnect } from './database/connectionMongoDB.js'
+import dbConnect from './database/connectionMongoDB.js'
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000
+const app = express()
 
 app.use(express.json())
 
@@ -15,4 +16,4 @@ app.listen(PORT, () => {
     console.log('API lista por el puerto', PORT)
 })
 
-module.exports = app;
+
--- database/connectionMongoDB.js
+++ database/connectionMongoDB.js
@@ -1,20 +1,23 @@
 //mongodb://localhost:27017
 
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 
-const dbConnect = () => {
-    const DB_URI = process.env.DB_URI
-    mongoose.connect(DB_URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    }, (err, res) =>{
-        if (!err){
-            console.log('Conexión correcta')
-        } else {
-            console.log('Error de conexión')
+const dbConnect = async () => {
+        try{
+            const DB_URI = process.env.DB_URI
+            await mongoose.connect(DB_URI, {
+                useNewUrlParser: true,
+                useUnifiedTopology: true
+            })
+            console.log('Conexion exitosa')
+        } catch(error){
+            console.log('Error de conexion', error)
         }
-    })
+    
 }
 
-module.exports = { dbConnect }
+export default dbConnect
